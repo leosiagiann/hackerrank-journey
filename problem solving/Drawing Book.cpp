6b6918177cc4bd36179b274deff3a161059d354e
Leonardo Siagian <if319017@students.del.ac.id>
2022-03-29 23:20:15 +0700

    Drawing Book Sollution

---
+++ problem solving/Drawing Book.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+int pageCount(int n, int p)
+{
+    // 6 -> 4,5 -> 2,3 -> 1
+    // 11,10 -> 9,8 -> 7,6 -> 5,4 -> 2,3 -> 1
+    int total;
+    if (n - p > p - 1)
+    {
+        total = p / 2;
+    }
+    else
+    {
+        (n % 2 == 0) ? ++n : n;
+        total = (n - p) / 2;
+    }
+    return total;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string p_temp;
+    getline(cin, p_temp);
+
+    int p = stoi(ltrim(rtrim(p_temp)));
+
+    int result = pageCount(n, p);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
+}
